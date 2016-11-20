b9174d9b3887aa7c9000fc566269e2caa5d4444c
kfrn <knfrances@gmail.com>
2016-11-21 12:07:33 +1300

    passes add todo test

--- reducer.js
+++ reducer.js
@@ -4,9 +4,17 @@ module.exports = reducer
 
 
 function reducer(state, action) {
+  const { id, text, completed } = action.payload
   newState = clone(state) // ensures that the reducer does not alter the state
   switch(action.type) {
     case 'ADD_TODO':
-      
+      newState.todos.push({
+        id: id,
+        text: text,
+        completed: completed
+        })
+          return newState
+    default:
+      return newState
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const test = require('tape')
+const freeze = require('deep-freeze')
 const reducer = require('../reducer')
 
 
@@ -9,20 +10,40 @@ test('Hello world test', (t) => {
 
 test('to add a new todo', (t) => {
   //arrange
+  const state = {
   todos: [
-    {
-      id: 0,
-      text: 'Learn web dev',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Write Cool stuff',
-      completed: false
-    }
-  ]
+      {
+        id: 0,
+        text: 'Learn web dev',
+        completed: false
+      }
+    ]
+  }
 
+  const expected = {
+    todos: [
+      {
+        id: 0,
+        text: 'Learn web dev',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Write Cool stuff',
+        completed: false
+      }
+    ]
+  }
+
+  const action = {type: 'ADD_TODO', payload: {id: 1, text: 'Write Cool stuff', completed: false}}
+
+  freeze(state)
+  freeze(action)
 
   //assert
+  const actual = reducer(state, action)
+
   //act
+  t.deepEqual(actual, expected, 'Succesfully adds a todo')
+  t.end()
 })
