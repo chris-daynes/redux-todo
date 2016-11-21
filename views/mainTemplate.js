96f7687f013ae0f5c230db3a04b63dea0963e2ae
chris-daynes <chrisdaynes@gmail.com>
2016-11-22 00:44:44 +1300

    successfully adds a new todo

--- views/mainTemplate.js
+++ views/mainTemplate.js
@@ -5,23 +5,36 @@ const html = require('yo-yo')
 module.exports = mainTemplate
 
 function mainTemplate(state, dispatch) {
+
+
+
   return html`
     <div>
       <h1>Get these things done!</h1>
       ${state.todos.map((todo, index) => Todo(todo, dispatch, index)) }
       <div>
         <input type='text'>
-        <button>Click to add todo</button>
+        <button onclick=${ () => addTodo(dispatch) }>Click</button>
       </div>
     </div>
   `
 }
 
+function addTodo (dispatch) {
+  const todo = document.querySelector('input').value
+  console.log('this is the todo ', todo);
+  return dispatch({
+    type: 'ADD_TODO',
+    payload: {text: todo}
+  })
+}
 
 function Todo (todoItem, dispatch, index) {
+  const customClass = todoItem.completed ? 'done' : ''
   return html`
     <div>
       <li
+        class=${customClass}
         onclick=${ () => dispatch({type: 'TOGGLE_TODO', payload: index})}
       >
         ${todoItem.id} ${todoItem.text} Completed: ${todoItem.completed}
